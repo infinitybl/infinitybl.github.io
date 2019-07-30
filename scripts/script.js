cbbf86367c582dd3e0cff59957caf646b3be3675
infinitybl <10676085+infinitybl@users.noreply.github.com>
2019-07-30 01:54:30 -0400

    Added navbar animations and list-group

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,32 @@
-/* Update copyright year */
+/* Update copyright year with the Date object */
 var date = new Date();
 document.querySelector(".copyright-year").textContent = date.getFullYear();
 
-/*  animateHeadings() function animates all the h1 and h2 headings on the webpage
-    each time it's called.
-    Input: Respective string indicating the animation.
+/*  animateElement() function animates the parameter element with the respective string.
+    Input: The element selector string and the respective string indicating the animation.
     Output: Nothing.
 */
-function animateHeadings(animation) {
-    /* Add the animated and the respective animation clases with web broweser support and remove it once
-       done so it can repeat the animation next time */
-    $("h1, h2").addClass("animated " + animation).one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function() {
-        $("h1, h2").removeClass("animated " + animation);
+function animateElement(element, animation) {
+    /* Add animated and the respective animation classes with web browser support, 
+       and remove it once done so it can repeat the animation next time */
+    $(element).addClass("animated " + animation).one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function() {
+        $(element).removeClass("animated " + animation);
     });
 }
 
-/* Animate headings once webpage loads */
+/* Animate the About page section headings once webpage loads */
 $(document).ready(function() {
-    animateHeadings("rubberBand");
+    animateElement("#about h1, #about h2", "rubberBand");
 });
 
-/* Animate headings every time navigation bar is clicked */
+
 $(".nav-link").on("click", function() {
     $(".navbar-collapse").collapse("hide"); /* Hide navigation bar right away after click */
-    animateHeadings("rubberBand");
-});
 
+    /* Animate headings every time a navigation link is clicked */
+    /* Target only the h1 and h2 headings of the respective page section (determined from what
+       navigation link the user pressed) to animate */
+    var headingsSelector = $(this).attr("href") + " h1 , " + $(this).attr("href") + " h2";
+    animateElement(headingsSelector, "rubberBand");
+});
 
